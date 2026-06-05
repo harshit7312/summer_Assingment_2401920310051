6d5db891cb26047211050b2992b798f66932d9ba
harshit7312 <devikasinghkanpur@email.com>
2026-06-05 18:03:02 +0530

    week 1 Day 5 solution

---
+++ Week_1/Day_5/validpalindrome.cpp
@@ -0,0 +1,38 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+class Solution {
+public:
+    bool isAlphaNum(char ch){
+        if((ch>='0'&& ch<='9') ||(tolower(ch)>='a'&& tolower(ch)<='z') )  {
+            return true;
+        }    
+        return false;    
+    }
+    bool isPalindrome(string s) {
+        int st = 0;
+        int e = s.length()-1;
+        while(st<=e){
+
+        if(!isAlphaNum(s[st])){
+            st++; continue;
+        }
+        if(!isAlphaNum(s[e])){
+            e--;continue;
+        }
+
+            if(tolower(s[st])!=tolower(s[e])){
+                return false;
+            }
+            else{
+                st++;
+                e--;
+            }
+        }
+        return true;
+        
+    }
+};
