14645ec82b90f32df6fe00f575d42bf1d960e340
harshit7312 <devikasinghkanpur@email.com>
2026-06-10 23:29:47 +0530

    Week 2 Day 3 solution

---
+++ Week_2/Day_3/repeated substring.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        string t = s + s;
+        return t.substr(1, t.size() - 2).find(s) != string::npos;
+    }
+};
